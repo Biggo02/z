936a741b22b492943198336cdea88ca216b41dd7
Biggo02 <o19549991@gmail.com>
2026-08-29 17:28:02 +0200

    feat: expose property draft autosave endpoint

--- properties/urls.py
+++ properties/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .autosave import property_autosave
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('properties/create/', views.property_create, name='property_create'),
+    path('properties/autosave/', property_autosave, name='property_autosave'),
     path('properties/<str:property_id>/edit/', views.property_edit, name='property_edit'),
     path('properties/<str:property_id>/', views.property_detail, name='property_detail'),
 ]
