c1d25ff31fd2203d09dd85f1a1b8420e5e3efa1b
Biggo02 <o19549991@gmail.com>
2026-08-30 12:59:48 +0200

    fix: utiliser le nouvel accueil Fasthome

--- properties/urls.py
+++ properties/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 from .autosave import property_autosave
+from .home_views import home_reference
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', home_reference, name='home'),
     path('properties/create/', views.property_create, name='property_create'),
     path('properties/autosave/', property_autosave, name='property_autosave'),
     path('properties/<str:property_id>/edit/', views.property_edit, name='property_edit'),
